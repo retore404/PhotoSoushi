93b1c18fcc66f624f98329cf3e35c21f0c8ab8cc
retore <44674058+retore404@users.noreply.github.com>
2020-05-10 13:33:04 +0900

    update:#25/ブレークポイント変更

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
     <footer>
         <div id="content-wrap" class="container-fluid">
-            <div class="col-md-6 offset-md-3 row">
+            <div class="col-xl-6 offset-xl-3 row">
                 <!--フッターのウィジェットスペース-->
                     <!--フッターのウィジェットスペース(左)-->
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-xl-6 col-sm-12">
                         <?php if ( dynamic_sidebar('footer_widget1') ) : else : endif; ?>
                     </div>
                     <!--フッターのウィジェットスペース(右)-->
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-xl-6 col-sm-12">
                         <?php if ( dynamic_sidebar('footer_widget2') ) : else : endif; ?>
                     </div>
             </div>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <section id="content">
         <div id="content-wrap" class="container-fluid">
             <div class="row">
-            <div class="col-md-6 offset-md-3 col-sm-12">
+            <div class="col-xl-6 offset-xl-3 col-sm-12">
                 <div class="section-title">
                     <h3 class="post-section">Posts.</h3>
                 </div>
@@ -12,21 +12,21 @@
                             <div class="post-each">
                                 <a href="<?php the_permalink(); ?>">
                                     <img src="<?php echo catch_first_image(); ?>" alt="<?php the_title(); ?>"  class="hover" />                                
-                                </a><br>
+                                </a>
                                 <span class="post-date"><?php echo get_the_date( 'Y-m-d' ); ?></span><br>
                                 <a href="<?php the_permalink(); ?>"><span class="post-title"><?php the_title(); ?></span></a><br>
                                 <span class="post-excerpt"><?php the_excerpt(); ?></span>
                             </div>
                         </div>                                                
                     <?php endwhile; endif; ?>
-                    <div class="col-md-12 row">
-                        <div class="col-md-4 offset-md-4" style="text-align:center;">
+                    <div class="col-sm-12 row">
+                        <div class="col-sm-4 offset-sm-4" style="text-align:center;">
                         <?php if( function_exists("the_pagination") ) the_pagination(); ?>
                         </div>
                     </div>
                 </div>
                 <!--メイン領域下のウィジェットスペース-->
-                <div class="col-md-12">
+                <div class="col-xl-12">
                     <?php if ( dynamic_sidebar('main_widget1') ) : else : endif; ?>
                 </div>
             </div>
