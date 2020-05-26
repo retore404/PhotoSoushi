caf63a8f97fff1dc925c8943f150d6630a61cb97
retore <44674058+retore404@users.noreply.github.com>
2020-05-26 21:43:49 +0900

    update: コメントフォームの順序変更

--- functions.php
+++ functions.php
@@ -96,3 +96,13 @@ function the_pagination() {
     ) );
     echo '</nav>';
   }
+
+  // コメントフォームの順序変更
+  function move_comment_field_to_bottom( $fields ) {
+    $comment_field = $fields['comment'];
+    unset( $fields['comment'] );
+    $fields['comment'] = $comment_field;
+    
+    return $fields;
+  }
+  add_filter( 'comment_form_fields', 'move_comment_field_to_bottom' );
