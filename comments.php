dfd4eccc188b4d3ad1437fdf3e6a1cedc08a782f
retore <44674058+retore404@users.noreply.github.com>
2020-05-26 21:42:09 +0900

    fix #31 : 名前・サイト入力欄の横幅指定を削除

--- comments.php
+++ comments.php
@@ -16,10 +16,10 @@
     $comments_args = array(
         'fields' => array(
             'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><br> ' .
-                        '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
+                        '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '""' . $aria_req . ' /></p>',
             'email'  => '',
 	        'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label><br> ' .
-		    			'<input id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>',
+		    			'<input id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '"/></p>',
 		),
 		'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
 	);
@@ -27,4 +27,3 @@
   comment_form($comments_args);
 ?>
 </div>
-<hr>
