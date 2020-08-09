2cf2ec69b2d297c7e55f1b7a75ec770339019a16
retore <44674058+retore404@users.noreply.github.com>
2020-08-09 22:47:28 +0900

    modify: フッターのウィジェットのブレークポイントを修正

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
                 <div class="col-xl-6 offset-xl-3 row">
                     <!--フッターのウィジェットスペース-->
                     <!--フッターのウィジェットスペース(左)-->
-                    <div class="col-xl-6 col-sm-12">
+                    <div class="col-sm-6 col-xs-12">
                         <?php if ( dynamic_sidebar('footer_widget1') ) : else : endif; ?>
                     </div>
                     <!--フッターのウィジェットスペース(右)-->
-                    <div class="col-xl-6 col-sm-12">
+                    <div class="col-sm-6 col-xs-12">
                         <?php if ( dynamic_sidebar('footer_widget2') ) : else : endif; ?>
                     </div>
                 </div>
