921347f3569daa228d030cbf1cd958394dbda1fc
retore <44674058+retore404@users.noreply.github.com>
2020-08-09 22:48:30 +0900

    modify: 検索フォームのデザイン変更

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-  <div><label class="screen-reader-text" for="s">Search for:</label>
+  <div>
     <input type="text" value="" name="s" id="s" style="max-width: 100%;"/>
     <input type="submit" id="searchsubmit" value="Search" />
   </div>
