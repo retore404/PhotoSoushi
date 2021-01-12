e7c6104cfbbc173531d64a5ae8c9a9d2b2dd4104
retore <44674058+retore404@users.noreply.github.com>
2021-01-12 23:29:56 +0900

    update: フッターウィジェット部のGridレイアウト化

--- single.php
+++ single.php
@@ -67,17 +67,15 @@
             </section>
             <!--フッタ開始部-->
             <footer>
-                <div class="grid-container-footer">
-                    <div class="col-xl-6 offset-xl-3 row widget-wrapper">
-                        <!--フッターのウィジェットスペース-->
-                        <!--フッターのウィジェットスペース(左)-->
-                        <div class="col-sm-6 col-xs-12">
-                            <?php if ( dynamic_sidebar('footer_widget1') ) : else : endif; ?>
-                        </div>
-                        <!--フッターのウィジェットスペース(右)-->
-                        <div class="col-sm-6 col-xs-12">
-                            <?php if ( dynamic_sidebar('footer_widget2') ) : else : endif; ?>
-                        </div>
+                <div class="grid-container-footer widget-wrapper">
+                    <!--フッターのウィジェットスペース-->
+                    <!--フッターのウィジェットスペース(左)-->
+                    <div>
+                        <?php if ( dynamic_sidebar('footer_widget1') ) : else : endif; ?>
+                    </div>
+                    <!--フッターのウィジェットスペース(右)-->
+                    <div>
+                        <?php if ( dynamic_sidebar('footer_widget2') ) : else : endif; ?>
                     </div>
                 </div>
             </footer>
