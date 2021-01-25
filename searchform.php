e90b0ec927dc1274b246995789c4b93eb236ce5d
retore <44674058+retore404@users.noreply.github.com>
2021-01-25 23:00:06 +0900

    image管理用ブランチの作成

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+        <input type="text" value="" name="s" id="s" style="max-width: 100%;"/>
+        <input type="submit" id="searchsubmit" value="Search" />
+    </div>
+</form>
