5985727a8050b8c1e9362f724644e33ed0d9c418
retore <44674058+retore404@users.noreply.github.com>
2021-02-16 08:03:20 +0900

    fix #103: デバッグモードON時のエラーメッセージ解消

--- comments.php
+++ comments.php
@@ -13,13 +13,14 @@
 </div>
 <div id="comment-form">
     <?php
+	    $aria_req = ( $req ? " aria-required='true'" : '' );
         $comments_args = array(
             'fields' => array(
                 'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><br> ' .
                             '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '""' . $aria_req . ' /></p>',
                 'email'  => '',
     	        'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label><br> ' .
-    		    			'<input id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '"/></p>',
+    		    			'<input id="url" name="url" type="url"' . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '"/></p>',
     		),
     		'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
     	);
