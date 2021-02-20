1388197bf49c7f04222e3e3ab7dcc34c0b21e65b
retore <44674058+retore404@users.noreply.github.com>
2021-02-20 09:44:08 +0900

    update: 個別記事ページのタグセマンティクス化

--- single.php
+++ single.php
@@ -2,11 +2,11 @@
             <main id="content">
                 <?php if(have_posts()): the_post(); ?>
                     <article class="grid-container-article">
-                        <div id="single-date"><?php echo get_the_date(); ?></div>
+                        <span id="single-date"><?php echo get_the_date(); ?></span>
                         <h3 id="single-title"><?php the_title(); ?></h3>
-                            <!--自動補正ありの本文-->
-                            <?php the_content(); ?>
-                        <div id="single-article-info">
+                        <!--自動補正ありの本文-->
+                        <?php the_content(); ?>
+                        <section id="single-article-info">
                             <?php if(has_category() ): ?>
                                 <div class="single-category-tags-wrapper"><span class="single-category-tags">Category: <?php if (the_category(', '))  the_category(); ?></span></div>
                             <?php endif; ?>
@@ -25,22 +25,22 @@
                                     ?>
                                 </span></div>
                             <?php endif; ?>
-                        </div>
-                        <div id="share">
+                        </section>
+                        <section id="share">
                             <span>Share.</span><br>
                             <a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo urlencode(get_the_title() . " | " . get_bloginfo('name') . "\n" ); ?>" target="_blank"><i class="fab fa-twitter"></i></a>
                             <a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank"><i class="fa fa-hatena"></i></a>
-                        </div>
-                        <div id="comments">
+                        </section>
+                        <section id="comments">
                             <?php
                                 comments_template();
                             ?>
-                        </div>
-                        <div id="post-links">
+                        </section>
+                        <section id="post-links">
                             <?php next_post_link('%link', '<< next'); ?>
                             |
                             <?php previous_post_link('%link', 'prev >>'); ?>
-                        </div>
+                        </section>
                     </article>
                 <?php endif; ?>
             </main>
