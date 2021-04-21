a2ee94e576c55fffab246341bbd734c96b633704
retore <44674058+retore404@users.noreply.github.com>
2021-04-21 23:13:31 +0900

    modify: icon配置修正

--- single.php
+++ single.php
@@ -47,11 +47,11 @@ get_header(); ?>
 						</section>
 						<section id="share">
 							<span>Share.</span><br>
-							<a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo rawurlencode( get_the_title() . ' | ' . get_bloginfo( 'name' ) . "\n" ); ?>" target="_blank"><span class="ps-sns-icon-twitter"></span></a>
-							<a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank"><span class="ps-sns-icon-hatenabookmark"></span></a>
-							<a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" ><span class="ps-sns-icon-line"></span></a>
-							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" ><span class="ps-sns-icon-facebook"></span></a>
-							<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>" ><span class="ps-sns-icon-pinterest"></span></a>
+							<a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo rawurlencode( get_the_title() . ' | ' . get_bloginfo( 'name' ) . "\n" ); ?>" target="_blank"><span class="share-icon ps-sns-icon-twitter"></span></a>
+							<a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank"><span class="share-icon ps-sns-icon-hatenabookmark"></span></a>
+							<a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" ><span class="share-icon ps-sns-icon-line"></span></a>
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" ><span class="share-icon ps-sns-icon-facebook"></span></a>
+							<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>" ><span class="share-icon ps-sns-icon-pinterest"></span></a>
 						</section>
 						<section id="comments">
 							<?php
