e89786fe42015ce767fcfb3d8e60799230d7b0aa
retore <44674058+retore404@users.noreply.github.com>
2021-05-05 12:24:57 +0900

    close #168: shareアイコンリンクへのaria-label指定追加

--- single.php
+++ single.php
@@ -46,13 +46,12 @@ get_header(); ?>
 							<?php endif; ?>
 						</section>
 						<section id="share">
-							<span>Share.</span><br>
-							<a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo rawurlencode( get_the_title() . ' | ' . get_bloginfo( 'name' ) . "\n" ); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-twitter"></span></a>
-							<a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-hatenabookmark"></span></a>
-							<a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-line"></span></a>
-							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-facebook"></span></a>
-							<a href="https://getpocket.com/save?url=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-pocket"></span></a>
-							<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-pinterest"></span></a>
+							<a href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo rawurlencode( get_the_title() . ' | ' . get_bloginfo( 'name' ) . "\n" ); ?>" target="_blank" class="share-icon" aria-label="share on twitter"><span class="ps-sns-icon-twitter"></span></a>
+							<a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank" class="share-icon" aria-label="bookmark on hatena"><span class="ps-sns-icon-hatenabookmark"></span></a>
+							<a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" target="_blank" class="share-icon" aria-label="share on LINE"><span class="ps-sns-icon-line"></span></a>
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" class="share-icon" aria-label="share on Facebook"><span class="ps-sns-icon-facebook"></span></a>
+							<a href="https://getpocket.com/save?url=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-pocket" aria-label="save on Pocket"></span></a>
+							<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>" target="_blank" class="share-icon"><span class="ps-sns-icon-pinterest" aria-label="share on Pinterest"></span></a>
 						</section>
 						<section id="comments">
 							<?php
