4755b2ab9b0e12eef937a7383075740147b5efc7
retore <44674058+retore404@users.noreply.github.com>
2021-05-08 20:42:16 +0900

    close #12: 検索フォームスタイル変更

--- searchform.php
+++ searchform.php
@@ -13,6 +13,9 @@
 
 <form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="s" class="visually-hidden">Search: </label>
-	<input type="text" value="" name="s" id="s" style="max-width: 100%;" />
-	<input type="submit" id="searchsubmit" value="Search" />
+	<h3>Search.</h3>
+	<section class="search-field-wrapper">
+		<span class="ps-icon-search"></span>
+		<input type="text" value="" placeholder="Search." name="s" id="s" class="search-field" />
+	</section>
 </form>
