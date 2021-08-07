54f2202dcb6c59292e0858f2d168692c916e0171
retore <44674058+retore404@users.noreply.github.com>
2021-08-07 14:54:05 +0900

    WIP

--- header.php
+++ header.php
@@ -16,7 +16,14 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
-		<?php do_action( 'ps_img_metadata_action_hook' ); ?>
+		<?php
+		// URLの設定.
+		$url = catch_first_image( 'png' ); // 画像のURLを格納.
+		echo '<meta property="og:image" content="' . esc_url( $url ) . '">' . "\n";
+		if ( substr( $url, 0, 5 ) === 'https' ) { // 取得した画像のURLがhttpsから始まるとき，secure_urlとしても指定.
+			echo '<meta property="og:image:secure_url" content="' . esc_url( $url ) . '">' . "\n";
+		}		
+		?>
 	</head>
 	<body>
 		<section id="grid-container-root">
